bfa2ebdaab0b73cfbc4058c291e945c4f76a846e
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-10-06 22:07:06 +0800

    mixin

--- src/main/java/com/happyrespawnanchor/resharpedrenderfixpatch/mixin/BladeRenderStateMixin.java
+++ src/main/java/com/happyrespawnanchor/resharpedrenderfixpatch/mixin/BladeRenderStateMixin.java
@@ -1,7 +1,12 @@
 package com.happyrespawnanchor.resharpedrenderfixpatch.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import mods.flammpfeil.slashblade.client.renderer.model.obj.WavefrontObject;
 import mods.flammpfeil.slashblade.client.renderer.util.BladeRenderState;
 import net.minecraft.Util;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,10 +16,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static mods.flammpfeil.slashblade.client.renderer.util.BladeRenderState.renderOverrided;
 
 @Mixin(BladeRenderState.class)
-public class BladeRenderStateMixin {
+public abstract class BladeRenderStateMixin {
+    @Shadow public abstract void renderOverrided(ItemStack stack, WavefrontObject model, String target, ResourceLocation texture, PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn);
+
     @Inject(at = @At("TAIL"), method = "renderOverrided*", remap = false)
     public void renderOverridedMixin(CallbackInfo ci) {
         renderOverrided(stack, model, target, texture, matrixStackIn, bufferIn,
-        packedLightIn, Util.memoize(BladeRenderState::getSlashBladeBlend), true);
+        packedLightIn, Util.memoize(BladeRenderState::getSlashBladeBlend), true;
+
     }
 }
