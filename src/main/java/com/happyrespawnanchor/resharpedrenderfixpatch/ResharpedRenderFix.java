eb18243f0077dd83f6b3172955e82cdee9c2d65c
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-10-07 11:49:42 +0800

    fix & done

---
+++ src/main/java/com/happyrespawnanchor/resharpedrenderfixpatch/ResharpedRenderFix.java
@@ -0,0 +1,10 @@
+package com.happyrespawnanchor.resharpedrenderfixpatch;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.happyrespawnanchor.resharpedrenderfixpatch.ResharpedRenderFix.MOD_ID;
+
+@Mod(MOD_ID)
+public class ResharpedRenderFix {
+    public static final String MOD_ID = "resharped_renderfix_patch";
+}
