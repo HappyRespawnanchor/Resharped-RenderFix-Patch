efb98595f1ada9390a954d0fc27da6027d08544a
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-10-07 18:59:28 +0800

    add logo

--- src/main/java/com/happyrespawnanchor/resharpedrenderfixpatch/mixin/BladeRenderStateMixin.java
+++ src/main/java/com/happyrespawnanchor/resharpedrenderfixpatch/mixin/BladeRenderStateMixin.java
@@ -8,20 +8,15 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static mods.flammpfeil.slashblade.client.renderer.util.BladeRenderState.renderOverrided;
-
 @Mixin(BladeRenderState.class)
 public class BladeRenderStateMixin {
-
-
     @Inject(at = @At("TAIL"), method = "renderOverrided(Lnet/minecraft/world/item/ItemStack;Lmods/flammpfeil/slashblade/client/renderer/model/obj/WavefrontObject;Ljava/lang/String;Lnet/minecraft/resources/ResourceLocation;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", remap = false)
-    private static void renderOverridedMixin(ItemStack stack, WavefrontObject model, String target, ResourceLocation texture, PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, CallbackInfo ci) {
-         BladeRenderState.renderOverrided(stack, model, target, texture, matrixStackIn, bufferIn, packedLightIn, Util.memoize(BladeRenderState::getSlashBladeBlend), true);
+    private static void renderOverridedFix(ItemStack stack, WavefrontObject model, String target, ResourceLocation texture, PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, CallbackInfo ci) {
+        BladeRenderState.renderOverrided(stack, model, target, texture, matrixStackIn, bufferIn, packedLightIn, Util.memoize(BladeRenderState::getSlashBladeBlend), true);
 
     }
 }
